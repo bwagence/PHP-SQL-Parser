fb74e0a16562841b07dd192ef1ad0c4dfabf8f59
Kévin Sonnet <kevin.sonnet@bwagence.fr>
2020-09-02 18:26:41 +0200

    Add NATURAL Join in Builder

--- src/PHPSQLParser/builders/JoinBuilder.php
+++ src/PHPSQLParser/builders/JoinBuilder.php
@@ -31,25 +31,27 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * @author    André Rothe <andre.rothe@phosco.info>
  * @author    George Schneeloch <noisecapella@gmail.com>
  * @copyright 2010-2014 Justin Swanhart and André Rothe
  * @license   http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
  * @version   SVN: $Id$
- * 
+ *
  */
 
 namespace PHPSQLParser\builders;
 
+use PHPSQLParser\exceptions\UnsupportedFeatureException;
+
 /**
- * This class implements the builder for the JOIN statement parts (within FROM). 
+ * This class implements the builder for the JOIN statement parts (within FROM).
  * You can overwrite all functions to achieve another handling.
  *
  * @author  André Rothe <andre.rothe@phosco.info>
  * @author  George Schneeloch <noisecapella@gmail.com>
  * @license http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
- *  
+ *
  */
 class JoinBuilder {
 
@@ -68,6 +70,9 @@ class JoinBuilder {
         }
         if ($parsed === 'STRAIGHT_JOIN') {
             return " STRAIGHT_JOIN ";
+        }
+        if ($parsed === 'NATURAL') {
+            return " NATURAL JOIN ";
         }
         // TODO: add more
         throw new UnsupportedFeatureException($parsed);
